8d44007a84f53209fdcc310d3c78c0373c06aab8
NikitaTudakov <tudakov.nikita@gmail.com>
2023-11-14 16:40:41 +0200

    updated db config

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,17 +1,6 @@
 
 import { DataSourceOptions } from 'typeorm';
 
-// export const connectionOptions = {
-//     type: 'postgres',
-//     host: 'localhost',
-//     port: 5432,
-//     username: 'postgres',
-//     password: 'xyz1234',
-//     database: 'usermanagement',
-//     entities: ['dist/**/*.entity{.ts,.js}'],
-//     synchronize: true,
-// } as DataSourceOptions;
-
 export const connectionOptions = {
     type: 'postgres',
     entities: ['dist/**/*.entity{.ts,.js}'],
