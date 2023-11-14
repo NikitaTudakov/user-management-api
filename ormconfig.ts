3f519d5a9b079f64256fc96acb5efdabc92e0f3e
NikitaTudakov <tudakov.nikita@gmail.com>
2023-11-14 16:22:16 +0200

    added firebase hosting,updated db options

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,13 +1,22 @@
 
 import { DataSourceOptions } from 'typeorm';
 
+// export const connectionOptions = {
+//     type: 'postgres',
+//     host: 'localhost',
+//     port: 5432,
+//     username: 'postgres',
+//     password: 'xyz1234',
+//     database: 'usermanagement',
+//     entities: ['dist/**/*.entity{.ts,.js}'],
+//     synchronize: true,
+// } as DataSourceOptions;
+
 export const connectionOptions = {
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'xyz1234',
-    database: 'usermanagement',
     entities: ['dist/**/*.entity{.ts,.js}'],
     synchronize: true,
+    name: "default",
+    url: 'postgres://kthhkpam:H-OIGH4L2ZMHge8A6yt57TNZzdlAYrJs@trumpet.db.elephantsql.com/kthhkpam', 
+    logging: true,
 } as DataSourceOptions;
